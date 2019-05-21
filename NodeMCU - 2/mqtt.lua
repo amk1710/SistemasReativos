9a13cfc7d649c5934ea0a6a3bea64f145aacff43
PEDRO SOUSA MEIRELES <e1510962@THOR.LAB-GRAD>
2019-05-21 19:03:06 -0300

    Publishing messages with nodeMCU

--- NodeMCU - 2/mqtt.lua
+++ NodeMCU - 2/mqtt.lua
@@ -1,3 +1,10 @@
+local led1 = 3
+local led2 = 6
+local sw1 = 1
+local sw2 = 2
+
+local m
+
 wificonf = {
     -- verificar ssid e senha
     ssid = "Rafa",
@@ -5,7 +12,7 @@ wificonf = {
     got_ip_cb = function (con)
         print ("meu IP: ", con.IP)
 
-        m = mqtt.Client("clientid", 120)
+        m = mqtt.Client("pedro", 120)
         -- conecta com servidor mqtt na máquina 'ipbroker' e porta 1883:
         m:connect("test.mosquitto.org", 1883, 0,
             -- callback em caso de sucesso
@@ -23,6 +30,27 @@ wificonf = {
                     -- fç chamada qdo inscrição ok:
                     function (client)
                         print("subscribe success")
+                        gpio.mode(led1, gpio.OUTPUT)
+                        gpio.mode(led2, gpio.OUTPUT)
+                        
+                        gpio.write(led1, gpio.LOW)
+                        gpio.write(led2, gpio.LOW)
+                        
+                        gpio.mode(sw1,gpio.INT,gpio.PULLUP)
+                        gpio.mode(sw2,gpio.INT,gpio.PULLUP)
+                        
+                        function pressedButton1()
+                            print("Apertei botao 1")
+                            m:publish("apertou-tecla", "apertei1", 0, 1)
+                        end
+                        
+                        function pressedButton2()
+                            print("Apertei botao 2")
+                            m:publish("apertou-tecla", "apertei2", 0, 1)
+                        end
+                        
+                        gpio.trig(sw1, "down", pressedButton1)
+                        gpio.trig(sw2, "down", pressedButton2)
                     end,
                     --fç chamada em caso de falha:
                     function(client, reason)
