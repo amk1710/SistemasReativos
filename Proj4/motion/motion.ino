b94a9f332253d0e42fbae5935cd734c0ac969389
Pedro Sousa Meireles <psmeireles25@gmail.com>
2019-06-19 12:15:33 -0300

    Arduino now detects environment light

--- Proj4/motion/motion.ino
+++ Proj4/motion/motion.ino
@@ -5,7 +5,8 @@ unsigned long time1 = 0;
 unsigned long time2 = 0;
 unsigned long timeSerial = 0;
 unsigned long timeDetected = 0;
-int lightTolerance = 250;
+int lightTolerance1 = 250;
+int lightTolerance2 = 250;
 
 void setup() {
   // put your setup code here, to run once:
@@ -13,6 +14,20 @@ void setup() {
   pinMode(light2, INPUT);
   Serial.begin(9600);
   Serial.setTimeout(500);
+
+  int lightValue1 = analogRead(light1);
+  int lightValue2 = analogRead(light2);
+  for(int i = 0; i < 4; i++){
+    delay(500);
+    lightValue1 += analogRead(light1);
+    lightValue2 += analogRead(light2);
+  }
+
+  lightTolerance1 = lightValue1/10;
+  lightTolerance2 = lightValue2/10;
+
+  Serial.println("Tol1: " + String(lightTolerance1));
+  Serial.println("Tol2: " + String(lightTolerance2));
 }
 
 void loop() {
@@ -21,28 +36,27 @@ void loop() {
   int lightValue2 = analogRead(light2);
 
 
-  if(lightValue1 < lightTolerance && millis() - timeDetected > 1000){
+  if(lightValue1 < lightTolerance1 && millis() - timeDetected > 1000){
     time1 = millis();
   }
-  if(lightValue2 < lightTolerance && millis() - timeDetected > 1000){
+  if(lightValue2 < lightTolerance2 && millis() - timeDetected > 1000){
     time2 = millis();
   }
 
   if(time1 > time2 && time1 - time2 < 500 && millis() - timeDetected > 1000){
     // right to left
-    Serial.println("Swipe right to left");
-    Serial.println("1: " + String(lightValue1) + "\t2: " + String(lightValue2));
-    Serial.println("Times: " + String(time1) + "\t" + String(time2));
-    
+    Serial.println("left");
+//    Serial.println("1: " + String(lightValue1) + "\t2: " + String(lightValue2));
+//    Serial.println("Times: " + String(time1) + "\t" + String(time2));
     timeDetected = millis();
     time1 = 0;
     time2 = 0;
   }
   else if(time1 < time2 && time2 - time1 < 500 && millis() - timeDetected > 1000){
     // left to right
-    Serial.println("Swipe left to right");
-    Serial.println("1: " + String(lightValue1) + "\t2: " + String(lightValue2));
-    Serial.println("Times: " + String(time1) + "\t" + String(time2));
+    Serial.println("right");
+//    Serial.println("1: " + String(lightValue1) + "\t2: " + String(lightValue2));
+//    Serial.println("Times: " + String(time1) + "\t" + String(time2));
     
     timeDetected = millis();
     time1 = 0;
